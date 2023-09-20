7d27dd480313661ecfc8b50c10e1483ea6777e09
Josue Roque <josueroque@yahoo.com>
2023-09-20 17:18:20 -0600

    Add all employee endpoints

--- routes/v1/employees/controller/employees.js
+++ routes/v1/employees/controller/employees.js
@@ -1,18 +1,61 @@
 const { Employee } = require("../../../../models/index");
 
 module.exports = {
+  async delete(req, res) {
+    const response = await Employee.destroy({
+      where: {
+        Id: req.params.id,
+      },
+    });
+    console.log(response);
+    return response;
+  },
+
+  async update(req, res) {
+    const response = await Employee.update(req.body.employee, {
+      where: {
+        Id: req.params.id,
+      },
+    });
+    return response;
+  },
+
+  async create(req, res) {
+    const response = await Employee.create(req.body.employee);
+    return response;
+  },
+
+  async findOne(req, res) {
+    let employees = await Employee.findOne({
+      attributes: [
+        "firstName",
+        "lastName",
+        "email",
+        "phoneNumber",
+        "email",
+        "documentNumber",
+        "id",
+        "gender",
+        "active",
+        "positionId",
+      ],
+      where: { Id: req.params.id },
+    });
+    return employees;
+  },
+
   async listAll(req, res) {
     let employees = await Employee.findAll({
       attributes: [
-        "FirstName",
-        "LastName",
-        "Email",
-        "PhoneNumber",
-        "Email",
-        "DocumentNumber",
-        "Id",
-        "Gender",
-        "Active",
+        "firstName",
+        "lastName",
+        "email",
+        "phoneNumber",
+        "email",
+        "documentNumber",
+        "id",
+        "gender",
+        "active",
       ],
       include: {
         association: "position",
--- routes/v1/employees/router.js
+++ routes/v1/employees/router.js
@@ -3,8 +3,42 @@ const router = express.Router();
 const employee = require("./controller/employees");
 
 router.get("/employees", async function (req, res) {
-  let employees = await employee.listAll();
+  const employees = await employee.listAll();
   res.status(200).json(employees);
+});
+
+router.get("/employees/:id", async function (req, res) {
+  const employees = await employee.findOne(req, res);
+  res.status(200).json(employees);
+});
+
+router.post("/employees", async function (req, res) {
+  try {
+    const response = await employee.create(req, res);
+    res.status(201).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something wrong happened!" });
+  }
+});
+
+router.put("/employees/:id", async function (req, res) {
+  try {
+    const response = await employee.update(req, res);
+    res.status(204).json(response);
+  } catch (error) {
+    res.status(500).json({ message: "Something wrong happened!" });
+  }
+});
+
+router.delete("/employees/:id", async function (req, res) {
+  try {
+    const response = await employee.delete(req, res);
+    res.status(200).json({ rowsDeleted: response });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something wrong happened!" });
+  }
 });
 
 module.exports = router;
